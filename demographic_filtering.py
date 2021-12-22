b673c2641e16d318b5e8e30fe08a0de407282ed9
spatel051 <77207491+spatel051@users.noreply.github.com>
2021-12-22 17:37:24 -0600

    Update demographic_filtering.py

--- demographic_filtering.py
+++ demographic_filtering.py
@@ -5,4 +5,4 @@ df = pd.read_csv('articles.csv')
 
 df = df.sort_values(['total_events'], ascending = [False])
 
-output = df[["url", "title", "lang", "total_events"]].head(20).values.tolist()
+output = df[["url", "title", "text", "lang", "total_events"]].head(20).values.tolist()
